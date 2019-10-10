94788800b02f826d45487d2801c730c6f5c32b5c
Phillip Nguyen <phil@givecampus.com>
2019-10-10 00:09:47 -0700

    flesh out set.js

--- src/set.js
+++ src/set.js
@@ -3,4 +3,18 @@ class Set {
     this.set = {};
     this.size = 0;
   }
+
+  add(data) {
+    if (!this.set.hasOwnProperty(data)) {
+      this.set[data] = true;
+      this.size++;
+    }
+  }
+
+  remove(data) {
+    if (this.set.hasOwnProperty(data)) {
+      delete this.set[data];
+      this.size--;
+    }
+  }
 }
