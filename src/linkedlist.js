eb8d0f91469301ff45cc3940ab4cf3f98e363440
Phillip Nguyen <phil@givecampus.com>
2019-10-14 22:15:39 -0700

    implemented linked list

--- src/linkedlist.js
+++ src/linkedlist.js
@@ -43,4 +43,49 @@ class LinkedList {
 
     return current.data;
   }
+
+  remove(index) {
+    if (index < 0 || index >= this.length) {
+      return null;
+    }
+
+    let current = this.head;
+
+    if (index === 0) {
+      this.head = current.next;
+    } else {
+      let counter = 0;
+      let previous = null;
+
+      while (counter < index) {
+        previous = current;
+        current = current.next;
+
+        counter++;
+      }
+
+      // Set previous node's next
+      // to the node after the deleted node
+      previous.next = current.next;
+      this.length--;
+    }
+
+    return current.data;
+  }
+
+  // return index of found node
+  // assume data is unique for simplication purposes
+  search(data) {
+    let index = 0;
+    let current = this.head;
+
+    while (current) {
+      if (data === current.data) return index;
+      current = current.next;
+
+      index++;
+    }
+
+    return null;
+  }
 }
